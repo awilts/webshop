0041ab221b97e4b500ef259a88e9c397c3deb64e
Alexander Wilts <alexander.wilts@rewe-digital.com>
2021-11-20 11:16:46 +0100

    added build&deploy github action

--- terraform/README.md
+++ terraform/README.md
@@ -2,3 +2,11 @@
 * Create service account with role "Editor"
 * Create JSON access key for this service account
 * put download key in this folder and save it as "terraform-credentials.json"
+
+TODO: scripts for above stuff
+
+* gcloud services enable run.googleapis.com
+TODO: enable other services
+
+to read:
+https://cloud.google.com/artifact-registry/docs/docker/quickstart?authuser=1
--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,33 @@ resource "google_container_registry" "registry" {
   project  = "wilts-webshop"
   location = "EU"
 }
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "europe-west3"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
