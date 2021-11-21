afb1d4b8d6abc5b9f77322b878c941e910c43729
Alexander Wilts <alexander.wilts@rewe-digital.com>
2021-11-21 18:51:46 +0100

    create reusable JsonLoader component

---
+++ src/components/JsonLoader.svelte
@@ -0,0 +1,17 @@
+<p>{objectName}:</p>
+<Tile style="margin: 0  0 15px">
+    {#if isLoading}
+        <Loading withOverlay={false} small/>
+    {:else if value}
+        <JSONTree value="{value}"/>
+    {/if}
+</Tile>
+
+<script>
+    import {Loading, Tile} from "carbon-components-svelte";
+    import JSONTree from 'svelte-json-tree';
+
+    export let objectName
+    export let isLoading
+    export let value
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,51 @@
 <div style="margin: 0 auto; max-width: 700px">
     <Header company="Wilts" platformName="Webshop"/>
-    <TextInput style="margin: 45px 0 15px" bind:value={product} labelText="Product" placeholder="Enter some product..."/>
+    <TextInput style="margin: 45px 0 15px" bind:value={product} labelText="Product"
+               placeholder="Enter some product..."/>
     <Button style="margin: 15px 0" disabled="{!product}" on:click={createOrder}>Create Order</Button>
-
-    <OrderLoader orderPromise="{orderPromise}"/>
-    <PickjobLoader orderId="{orderId}" pickjobsResponse="{pickjobsResponse}"/>
+    <JsonLoader objectName="Order" isLoading="{orderLoading}" value="{order}"/>
+    <JsonLoader objectName="Pickjob" isLoading="{pickjobLoading}" value="{pickjob}"/>
 </div>
 <script>
     import "carbon-components-svelte/css/white.css";
     import {sleep} from "../sleep";
     import {Button, Header, TextInput} from "carbon-components-svelte";
-    import OrderLoader from "../components/OrderLoader.svelte";
-    import PickjobLoader from "../components/PickjobLoader.svelte";
+    import JsonLoader from "../components/JsonLoader.svelte";
 
     let orderPromise
-    let orderId = undefined;
+    let orderLoading = false
+    let pickjobLoading = false
+    let order
+    let pickjob
     let product = '';
-    let pickjobsResponse = undefined;
 
     async function createOrder() {
+        orderLoading = true
         orderPromise = fetch('/api/orders', {
             method: "POST",
             body: {
                 "product": product
             }
         }).then(res => res.json())
-            .then(order => {
-                orderId = order.id
-                loadPickjob(orderId)
-                return order
+            .then(orderResponse => {
+                orderLoading = false
+                order = orderResponse
+                getPickjob(orderResponse.id)
+                return orderResponse
             })
     }
 
-    async function loadPickjob(orderId) {
-        while (pickjobsLoading()) {
-            await getPickjobs(orderId)
-            await sleep(500)
+    async function getPickjob(orderId) {
+        pickjobLoading = true
+        const pickjobResponse = await fetch('/api/pickjobs?orderId=' + orderId)
+            .then(res => res.json());
+        if(pickjobResponse?.total>0){
+            pickjobLoading = false
+            //TODO: type
+            pickjob = pickjobResponse.pickjobs[0]
+            return
         }
-    }
-
-    async function getPickjobs(orderId) {
-        pickjobsResponse = await fetch('/api/pickjobs?orderId=' + orderId).then(res => res.json())
-    }
-
-    function pickjobsLoading() {
-        if (!pickjobsResponse) return true
-        return pickjobsResponse.total === 0;
+        await sleep(500)
+        await getPickjob(orderId)
     }
 </script>
